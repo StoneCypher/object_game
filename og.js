6c975bd3f4f30c0b50cefe07cffdc041f7a59172
John Haugeland <stonecypher@gmail.com>
2019-09-07 12:45:53 -0700

    lol

---
+++ og.js
@@ -0,0 +1,93 @@
+
+const screenWidth    = 512,
+      screenHeight   = 384,
+      doggoBaseWidth = 40;
+
+const near_back  = 200,
+      near_front = 300;
+
+const rand         = n      => Math.floor(Math.random() * n),
+      rand_between = (x, y) => rand(y-x) + x;
+
+let target,
+    dogs;
+
+const seq = n =>
+  new Array(n).fill(false).map( (_, i) => i );  // seq(5) -> [0,1,2,3,4]
+
+const n_times = (n, doWhat) =>
+  seq(n).map(_ => doWhat());
+
+const pctToBack = y =>
+  (screenHeight - y) / screenHeight;
+
+// does the actual work of creating and attaching a doggo
+// doggo coordinate is upper left hand corner, le sigh
+function PlaceAt(x, y) {
+
+  let realToBack        = (screenHeight - y) / screenHeight,
+      scale             = 1 - (realToBack * 1);
+
+  console.log(scale);
+
+  let thisDoggo         = document.createElement('img');
+
+  thisDoggo.className   = 'doggo';
+  thisDoggo.src         = 'chihuahua.png';
+  thisDoggo.style.top   = `${y}px`;
+  thisDoggo.style.left  = `${x}px`;
+  thisDoggo.style.width = `${doggoBaseWidth * scale}px`;
+  thisDoggo.onclick     = receiveDoggoClick;
+
+  dogs.appendChild(thisDoggo);
+  ++doggoCount;
+
+}
+
+function receiveDoggoClick(evt) {
+  --doggoCount;
+  evt.srcElement.parentElement.removeChild(evt.srcElement);
+  if (doggoCount <= 0) {
+    target.className = 'replay';
+  }
+}
+
+function PlaceOnGrass() {
+  let x = rand(screenWidth - doggoBaseWidth),
+      y = rand_between(near_front, near_back);
+  PlaceAt(x, y);
+}
+
+function PlaceOnNearGrass() {
+  let x = rand(screenWidth - doggoBaseWidth),
+      y = rand_between(screenHeight - 50, near_front);
+  PlaceAt(x, y);
+}
+
+function place_doggos() {
+  n_times(10, PlaceOnGrass);
+  n_times(3,  PlaceOnNearGrass);
+}
+
+function enterGameMode() {
+  doggoCount       = 0;
+  dogs.innerHTML   = '';
+  target.className = 'game';
+  place_doggos();
+}
+
+function startup() {
+
+  target = document.getElementById('target');
+  dogs   = document.getElementById('dogs');
+
+  document.body.className = 'running';
+  target.className        = 'menu';
+
+  document.getElementById('begin').onclick = enterGameMode;
+
+}
+
+
+
+window.onload = startup;
